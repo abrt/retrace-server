df73bae2b4106b5839acbadec7a279dd064f4253
Marek Bryša <mbrysa@redhat.com>
2015-07-02 09:36:08 +0200

    lib: use sublogger for logging

--- src/lib/retrace.py
+++ src/lib/retrace.py
@@ -237,17 +237,21 @@ class RetraceWorkerError(RetraceError):
 def now():
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+
+logger = logging.getLogger(__name__)
+
+
 def log_info(msg):
-    logging.info("%23s %s" % (now(), msg))
+    logger.info("%23s %s" % (now(), msg))
 
 def log_debug(msg):
-    logging.debug("%22s %s" % (now(), msg))
+    logger.debug("%22s %s" % (now(), msg))
 
 def log_warn(msg):
-    logging.warn("%20s %s" % (now(), msg))
+    logger.warn("%20s %s" % (now(), msg))
 
 def log_error(msg):
-    logging.error("%22s %s" % (now(), msg))
+    logger.error("%22s %s" % (now(), msg))
 
 def lock(lockfile):
     try:
