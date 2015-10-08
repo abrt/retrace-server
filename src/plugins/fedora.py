b0bada96ed01bc2ebb3de8abc9215a03ab0f3f34
Richard Marko <rmarko@fedoraproject.org>
2015-10-08 15:40:34 +0200

    update fedora plugin

--- src/plugins/fedora.py
+++ src/plugins/fedora.py
@@ -5,23 +5,27 @@ abrtparser = re.compile("^Fedora release ([0-9]+) \(([^\)]+)\)$")
 guessparser = re.compile("\.fc([0-9]+)")
 repos = [
   [
-    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/releases/$VER/Everything/$ARCH/os/Packages/*",
-    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/development/$VER/$ARCH/os/Packages/*/*",
+    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/releases/$VER/Everything/$ARCH/os/Packages/*/*.rpm",
+    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/development/$VER/$ARCH/os/Packages/*/*.rpm",
   ],
   [
-    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/releases/$VER/Everything/$ARCH/debug/*",
-    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/development/$VER/$ARCH/debug/*/*",
+    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/releases/$VER/Everything/$ARCH/debug/*/*.rpm",
+    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/development/$VER/$ARCH/debug/*/*.rpm",
   ],
   [
-    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/$VER/$ARCH/*",
+    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/$VER/$ARCH/*.rpm",
+    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/$VER/$ARCH/*/*.rpm",
   ],
   [
-    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/$VER/$ARCH/debug/*",
+    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/$VER/$ARCH/debug/*.rpm",
+    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/$VER/$ARCH/debug/*/*.rpm",
   ],
   [
-    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/testing/$VER/$ARCH/*",
+    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/testing/$VER/$ARCH/*.rpm",
+    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/testing/$VER/$ARCH/*/*.rpm",
   ],
   [
-    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/testing/$VER/$ARCH/debug/*",
+    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/testing/$VER/$ARCH/debug/*.rpm",
+    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/testing/$VER/$ARCH/debug/*/*.rpm",
   ],
 ]
