c4159dd35444330f68a0599ac0f932b8438e8fed
Matej Marusak <marusak.matej@gmail.com>
2017-01-21 20:35:01 +0100

    Improve autogen to list and install dependencies
    Signed-off-by: Matej Marusak <marusak.matej@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,27 +1,72 @@
 #! /bin/sh
 
-mkdir -p m4
-echo "Creating m4/aclocal.m4 ..."
-test -r m4/aclocal.m4 || touch m4/aclocal.m4
+print_help()
+{
+cat << EOH
+Prepares the source tree for configuration
 
-echo "Running autopoint"
-autopoint --force || exit 1
+Usage:
+  autogen.sh [sydeps [--install-yum|--install-dnf]]
 
-echo "Running intltoolize..."
-intltoolize --force --copy --automake || exit 1
+Options:
 
-echo "Running aclocal..."
-aclocal || exit 1
+  sysdeps              prints out all dependencies
+    --install-yum      install all dependencies ('sudo yum install \$DEPS')
+    --install-dnf      install all dependencies ('sudo dnf install \$DEPS')
 
-echo "Running libtoolize..."
-libtoolize || exit 1
+EOH
+}
 
-echo "Running autoheader..."
-autoheader || exit 1
+build_depslist()
+{
+    DEPS_LIST=`grep "^\(Build\)\?Requires:" *.spec.in | grep -v "%{name}" | tr -s " " | tr "," "\n" | cut -f2 -d " " | grep -v "^abrt" | sort -u | while read br; do if [ "%" = ${br:0:1} ]; then grep "%define $(echo $br | sed -e 's/%{\(.*\)}/\1/')" *.spec.in | tr -s " " | cut -f3 -d" "; else echo $br ;fi ; done | tr "\n" " "`
+}
 
-echo "Running autoconf..."
-autoconf --force || exit 1
+case "$1" in
+    "--help"|"-h")
+            print_help
+            exit 0
+        ;;
+    "sysdeps")
+            build_depslist
 
-echo "Running automake..."
-automake --add-missing --force --copy || exit 1
+            if [ "$2" == "--install" ] || [ "$2" == "--install-dnf" ]; then
+                set -x verbose
+                sudo dnf install --setopt=strict=0 $DEPS_LIST
+                set +x verbose
+            elif [ "$2" == "--install-yum" ]; then
+                set -x verbose
+                sudo yum install $DEPS_LIST
+                set +x verbose
+            else
+                echo $DEPS_LIST
+            fi
+            exit 0
+        ;;
+    *)
+            mkdir -p m4
+            echo "Creating m4/aclocal.m4 ..."
+            test -r m4/aclocal.m4 || touch m4/aclocal.m4
 
+            echo "Running autopoint"
+            autopoint --force || exit 1
+
+            echo "Running intltoolize..."
+            intltoolize --force --copy --automake || exit 1
+
+            echo "Running aclocal..."
+            aclocal || exit 1
+
+            echo "Running libtoolize..."
+            libtoolize || exit 1
+
+            echo "Running autoheader..."
+            autoheader || exit 1
+
+            echo "Running autoconf..."
+            autoconf --force || exit 1
+
+            echo "Running automake..."
+            automake --add-missing --force --copy || exit 1
+        ;;
+esac
