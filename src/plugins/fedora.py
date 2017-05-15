a67a9a9f6d9ddb0fa4f27b048d174923118a911b
Matěj Kudera <matejkudera1@seznam.cz>
2017-05-15 15:15:36 +0200

    fedora C: 50, 0: Wrong hanging indentation

--- src/plugins/fedora.py
+++ src/plugins/fedora.py
@@ -47,7 +47,7 @@ repos = [
         "rsync://dl.fedoraproject.org/fedora-enchilada/linux/development/$VER/Everything/$ARCH/debug/tree/Packages/*/*.rpm",
     ],
     [
-    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/$VER/$ARCH/*.rpm",
+        "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/$VER/$ARCH/*.rpm",
     "rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/$VER/$ARCH/*/*.rpm",
   ],
   [
