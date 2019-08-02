abrt/retrace-server

commit 2199ab807462cc38e77b80b74628dfeb8109311f

Move ProcessCommunicateTimeout inside run_crash_cmdline
We have seen in times past where some crash commands may take a
long time to run.  Previously the ProcessCommunicateTimeout option
was introduced in becb4066 to address a runaway crash command.  That
patch was done very conservatively and only touched one crash call
where crash had periodically gone into an infinite loop.
However that patch was not a full fix and further use of
ProcessCommunicateTimeout was deemed future enhancement.

In reality many other crash commands may take a long time to execute
and there could be good reasons why ProcessCommunicateTimeout should
be set to a low value in a production environment.  Thus, we move
ProcessCommunicateTimeout inside run_crash_cmdline, as well as
refactor the other callers of crash to also call run_crash_commandline
and thus also have the safety of a runtime bound by
ProcessCommunicateTimeout.

Signed-off-by: Dave Wysochanski <dwysocha@redhat.com>

src/retrace/retrace.py
@@ -1733,11 +1733,19 @@ class RetraceTask:
         returncode = 0
         try:
             child = Popen(crash_start, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-            cmd_output = child.communicate(crash_cmdline.encode())[0]
+            t = 3600
+            if CONFIG["ProcessCommunicateTimeout"]:
+                t = CONFIG["ProcessCommunicateTimeout"]
+            cmd_output = child.communicate(crash_cmdline.encode(), timeout=t)[0]
         except OSError as err:
             log_warn("crash command: '%s' triggered OSError " %
                      crash_cmdline.replace('\r', '; ').replace('\n', '; '))
             log_warn("  %s" % err)
+        except TimeoutExpired as err:
+            child.kill()
+            raise Exception("WARNING: crash command: '%s' exceeded " + str(t) +
+                            " second timeout - damaged vmcore?" %
+                            crash_cmdline.replace('\r', '; ').replace('\n', '; '))
         except:
             log_warn("crash command: '%s' triggered Unknown exception %s" %
                      crash_cmdline.replace('\r', '; ').replace('\n', '; '))
@@ -1862,41 +1870,32 @@ class RetraceTask:
         # Obtain the list of modules this vmcore requires
         if chroot:
             with open(os.devnull, "w") as null:
-                child = Popen(["/usr/bin/mock", "--configdir", chroot, "shell",
-                               "--", "crash -s %s %s" % (vmcore, vmlinux)],
-                              stdin=PIPE, stdout=PIPE, stderr=null, encoding='utf-8')
+                crash_normal = ["/usr/bin/mock", "--configdir", chroot, "shell",
+                                "--", "crash -s %s %s" % (vmcore, vmlinux)]
         else:
-            child = Popen(crash_cmd + ["-s", vmcore, vmlinux], stdin=PIPE, stdout=PIPE, stderr=STDOUT,
-                          encoding='utf-8')
-        try:
-            t = 3600
-            if CONFIG["ProcessCommunicateTimeout"]:
-                t = CONFIG["ProcessCommunicateTimeout"]
-            stdout = child.communicate("mod\nquit", timeout=t)[0]
-        except TimeoutExpired:
-            child.kill()
-            raise Exception("WARNING: crash 'mod' command exceeded " + str(t) + " second timeout - damaged vmcore?")
-        if child.returncode == 1 and "el5" in kernelver.release:
+            crash_normal = crash_cmd + ["-s", vmcore, vmlinux]
+        stdout, returncode = self.run_crash_cmdline(crash_normal, "mod\nquit")
+        if returncode == 1 and "el5" in kernelver.release:
             log_info("Unable to list modules but el5 detected, trying crash fixup for vmss files")
             crash_cmd.append("--machdep")
             crash_cmd.append("phys_base=0x200000")
             log_info("trying crash_cmd = " + str(crash_cmd))
-            child = Popen(crash_cmd + ["-s", vmcore, vmlinux], stdin=PIPE, stdout=PIPE, stderr=STDOUT,
-                          encoding='utf-8')
-            stdout = child.communicate("mod\nquit")[0]
+            # FIXME: mock
+            crash_normal = crash_cmd + ["-s", vmcore, vmlinux]
+            stdout, returncode = self.run_crash_cmdline(crash_normal, "mod\nquit")
 
         # If we fail to get the list of modules, is the vmcore even usable?
-        if child.returncode:
-            log_warn("Unable to list modules: crash exited with %d:\n%s" % (child.returncode, stdout))
+        if returncode:
+            log_warn("Unable to list modules: crash exited with %d:\n%s" % (returncode, stdout))
             return vmlinux
 
         modules = []
         for line in stdout.splitlines():
             # skip header
-            if "NAME" in line:
+            if b"NAME" in line:
                 continue
 
-            if " " in line:
+            if b" " in line:
                 modules.append(line.split()[1])
 
         todo = []

commit 8e68eb5e0e289f48f0a71d229c46df66e9a53e98

Fix module extraction from kernel-debuginfo
Commit 2199ab8 broke module extraction because it did handle the
byte arrary return from run_crash_cmdline properly.
Rather than a string, run_crash_cmdline returns a byte array.  When
we are obtaining the list of module object files to extract from crash's
'mod' command, these should be in the form of strings so we can pass
them to 'cpio'.  The reason for the default byte array return on
run_crash_cmdline is found in the discussion of 7ea86bd:
1) some crash commands (not the 'mod' command) may return non-'utf-8'
characters (for example a command that reads a lot of
memory from a vmcore that may be incomplete or damaged)
2) often we save the output of crash commands to a file so we do not
care necessarily if we have non-'utf-8' characters.

Signed-off-by: Dave Wysochanski <dwysocha@redhat.com>

src/retrace/retrace.py
@@ -1890,12 +1890,12 @@ class RetraceTask:
             return vmlinux
 
         modules = []
-        for line in stdout.splitlines():
+        for line in stdout.decode('utf-8').splitlines():
             # skip header
-            if b"NAME" in line:
+            if "NAME" in line:
                 continue
 
-            if b" " in line:
+            if " " in line:
                 modules.append(line.split()[1])
 
         todo = []