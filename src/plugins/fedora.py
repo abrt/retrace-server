8c8d20b0d703c00bf6366d83af769f4675e56db5
Martin Kutlak <mkutlak@redhat.com>
2019-09-27 13:19:30 +0200

    plugins: Update fedora versionlist
    Show "stats" for 10 latest versions of Fedora.
    
    Signed-off-by: Martin Kutlak <mkutlak@redhat.com>

--- src/plugins/fedora.py
+++ src/plugins/fedora.py
@@ -7,30 +7,17 @@ displayrelease = "Fedora release"
 gdb_package = "gdb"
 gdb_executable = "/usr/bin/gdb"
 versionlist = [
-    "fc1",
-    "fc2",
-    "fc3",
-    "fc4",
-    "fc5",
-    "fc6",
-    "fc7",
-    "fc8",
-    "fc9",
-    "fc10",
-    "fc11",
-    "fc12",
-    "fc13",
-    "fc14",
-    "fc15",
-    "fc16",
-    "fc17",
-    "fc18",
-    "fc19",
-    "fc20",
-    "fc21",
     "fc22",
     "fc23",
     "fc24",
+    "fc25",
+    "fc26",
+    "fc27",
+    "fc28",
+    "fc29",
+    "fc30",
+    "fc31",
+    "fc32",
 ]
 
 # Find more details about Fedora Mirroring at:
