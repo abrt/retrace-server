c50ce20340abbebc096e4cd387c20b360362803b
Dave Wysochanski <dwysocha@redhat.com>
2020-06-09 17:29:11 -0400

    Fix group and other permissions on dmesg file
    The files inside the 'results' directory should be readable by
    all users in AuthGroup.
    
    Signed-off-by: Dave Wysochanski <dwysocha@redhat.com>
    Reported-by: Bud Brown <bubrown@redhat.com>

--- src/retrace/retrace.py
+++ src/retrace/retrace.py
@@ -934,6 +934,7 @@ class KernelVMcore:
             result = int(match.group(1))
             break
 
+        os.chmod(dmesg_path, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH)
         self._dump_level = result
         return result
 
