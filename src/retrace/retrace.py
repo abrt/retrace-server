c2badd4d373cddef0b7294b2dcc8cd91e72cf7d9
Matěj Grabovský <mgrabovs@redhat.com>
2022-02-10 14:49:36 +0100

    retrace: Split archs-related code out into separate module

--- src/retrace/retrace.py
+++ src/retrace/retrace.py
@@ -14,9 +14,10 @@ import urllib.request
 from pathlib import Path
 from signal import getsignal, signal, SIG_DFL, SIGPIPE
 from subprocess import DEVNULL, PIPE, STDOUT, TimeoutExpired, run
-from typing import Dict, Iterable, List, Optional, Set, Tuple, Union
+from typing import Iterable, List, Optional, Set, Tuple, Union
 import magic
 
+from .architecture import ARCH_MAP, ARCHITECTURES, get_canon_arch, guess_arch
 from .config import Config, PODMAN_BIN, PS_BIN
 from .util import (ARCHIVE_7Z,
                    ARCHIVE_BZ2,
@@ -75,7 +76,6 @@ SNAPSHOT_SUFFIXES = [".vmss", ".vmsn", ".vmem"]
 BUGZILLA_STATUS = ["NEW", "ASSIGNED", "ON_DEV", "POST", "MODIFIED", "ON_QA", "VERIFIED",
                    "RELEASE_PENDING", "CLOSED"]
 
-CORE_ARCH_PARSER = re.compile(r"core file,? .*(x86-64|80386|ARM|aarch64|IBM S/390|64-bit PowerPC)")
 PACKAGE_PARSER = re.compile(r"^(.+)-([0-9]+(\.[0-9]+)*-[0-9]+)\.([^-]+)$")
 
 REPODIR_NAME_PARSER = re.compile(r"^[^\-]+\-[^\-]+\-[^\-]+$")
@@ -112,28 +112,6 @@ STATUS = [
     "Post-processing downloaded file",
     "Calculating md5sum",
 ]
-
-ARCHITECTURES = {"src", "noarch", "i386", "i486", "i586", "i686", "x86_64",
-                 "s390", "s390x", "ppc", "ppc64", "ppc64le", "ppc64iseries",
-                 "armel", "armhfp", "armv5tel", "armv7l", "armv7hl",
-                 "armv7hnl", "aarch64", "sparc", "sparc64", "mips4kec",
-                 "ia64"}
-
-# armhfp is not correct, but there is no way to distinguish armv5/armv6/armv7 coredumps
-# as armhfp (RPM armv7hl) is the only supported now, let's approximate arm = armhfp
-
-# "arm" has been intentionally removed - when guessing architecture, it matches
-# "alarm" or "hdparm" and thus leads to wrong results.
-# As soon as plain "arm" needs to be supported, this needs to be solved properly.
-ARCH_MAP: Dict[str, Set[str]] = {
-    "i386": {"i386", "i486", "i586", "i686"},
-    "armhfp": {"armhfp", "armel", "armv5tel", "armv7l", "armv7hl", "armv7hnl"},
-    "x86_64": {"x86_64"},
-    "s390x": {"s390x"},
-    "ppc64": {"ppc64"},
-    "ppc64le": {"ppc64le"},
-    "aarch64": {"aarch64"},
-}
 
 PYTHON_LABEL_START = "----------PYTHON-START--------"
 PYTHON_LABEL_END = "----------PYTHON--END---------"
@@ -277,59 +255,6 @@ class RetraceWorkerError(RetraceError):
     def __init__(self, message: str = None, errorcode: int = 1):
         super().__init__(message)
         self.errorcode = errorcode
-
-
-def get_canon_arch(arch: str) -> str:
-    for canon_arch, derived_archs in ARCH_MAP.items():
-        if arch in derived_archs:
-            return canon_arch
-
-    return arch
-
-
-def guess_arch(coredump_path: Path) -> Optional[str]:
-    output = run(["file", str(coredump_path)], stdout=PIPE, encoding="utf-8",
-                 check=False).stdout
-    match = CORE_ARCH_PARSER.search(output)
-    if match:
-        if match.group(1) == "80386":
-            return "i386"
-        if match.group(1) == "x86-64":
-            return "x86_64"
-        if match.group(1) == "ARM":
-            # hack - There is no reliable way to determine which ARM
-            # version the coredump is. At the moment we only support
-            # armv7hl / armhfp - let's approximate arm = armhfp
-            return "armhfp"
-        if match.group(1) == "aarch64":
-            return "aarch64"
-        if match.group(1) == "IBM S/390":
-            return "s390x"
-        if match.group(1) == "64-bit PowerPC":
-            if "LSB" in output:
-                return "ppc64le"
-
-            return "ppc64"
-
-    result = None
-    lines = run(["strings", str(coredump_path)],
-                stdout=PIPE, stderr=STDOUT, encoding="utf-8",
-                check=False).stdout.splitlines()
-    for line in lines:
-        for canon_arch, derived_archs in ARCH_MAP.items():
-            if any(arch in line for arch in derived_archs):
-                result = canon_arch
-                break
-
-        if result is not None:
-            break
-
-    # "ppc64le" matches both ppc64 and ppc64le
-    # if file magic says little endian, fix it
-    if result == "ppc64" and "LSB" in output:
-        result = "ppc64le"
-
-    return result
 
 
 def get_supported_releases() -> List[str]:
