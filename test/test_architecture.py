5bc86c04018af3c4a51bb00c556e061823d119ee
Matěj Grabovský <mgrabovs@redhat.com>
2022-02-11 15:49:11 +0100

    test: Lint with Pylint; add test for architecture.py

---
+++ test/test_architecture.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from retrace.architecture import get_canon_arch
+
+
+class TestGetCanonArch(TestCase):
+    def test_i386(self):
+        self.assertEqual(get_canon_arch("i386"), "i386")
+        self.assertEqual(get_canon_arch("i686"), "i386")
+
+    def test_armhfp(self):
+        self.assertEqual(get_canon_arch("armhfp"), "armhfp")
+        self.assertEqual(get_canon_arch("armhfp"), "armv7hl")
+
+    def test_x86_64(self):
+        self.assertEqual(get_canon_arch("x86_64"), "x86_64")
+
+    def test_ppc64(self):
+        self.assertEqual(get_canon_arch("ppc64"), "ppc64")
+
+    def test_aarch64(self):
+        self.assertEqual(get_canon_arch("aarch64"), "aarch64")
+
+    def test_unknown(self):
+        self.assertEqual(get_canon_arch("unknown"), "unknown")
--- test/test_util.py
+++ test/test_util.py
@@ -3,29 +3,27 @@ from unittest import TestCase
 from retrace.util import human_readable_size
 
 
-class TestHumanReadableSize(TestCase):
-    CONTAINER_ID = "cefe728950018e14f00cc874d45259742e2925c6ab40e0044a4aedaf1514b660"
-
+class TestGetCanonArch(TestCase):
     def test_zero_bytes(self):
-        self.assertEqual(human_readable_size(0), '0.00 B')
+        self.assertEqual(human_readable_size(0), "0.00 B")
 
     def test_one_byte(self):
-        self.assertEqual(human_readable_size(1), '1.00 B')
+        self.assertEqual(human_readable_size(1), "1.00 B")
 
     def test_one_kilobyte(self):
-        self.assertEqual(human_readable_size(1000), '1000.00 B')
+        self.assertEqual(human_readable_size(1000), "1000.00 B")
 
     def test_one_kibibyte(self):
-        self.assertEqual(human_readable_size(1024), '1.00 kB')
+        self.assertEqual(human_readable_size(1024), "1.00 kB")
 
     def test_1025_bytes(self):
-        self.assertEqual(human_readable_size(1025), '1.00 kB')
+        self.assertEqual(human_readable_size(1025), "1.00 kB")
 
     def test_one_mebibyte(self):
-        self.assertEqual(human_readable_size(1024 * 1024), '1.00 MB')
+        self.assertEqual(human_readable_size(1024 * 1024), "1.00 MB")
 
     def test_five_mebibytes(self):
-        self.assertEqual(human_readable_size(5 * 1024 * 1024), '5.00 MB')
+        self.assertEqual(human_readable_size(5 * 1024 * 1024), "5.00 MB")
 
     def test_one_pebibyte(self):
-        self.assertEqual(human_readable_size(1024 ** 5), '1.00 PB')
+        self.assertEqual(human_readable_size(1024 ** 5), "1.00 PB")
